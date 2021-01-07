243b5bcf3a0641d78ae8f301661aeab235ff0d3a
Olga <olga_sprincean@yahoo.com>
2021-01-07 09:49:45 -0600

    wrote the ES2015 versions

--- enhancement.js
+++ enhancement.js
@@ -1,20 +1,30 @@
 //Same keys and values
-function createInstructor(firstName, lastName){
-    return {
-      firstName: firstName,
-      lastName: lastName
-    }
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+  /* Write an ES2015 Version */
+  const createInstructor = (firstName,lastName) => {
+      return {firstName,lastName}
   }
-  
   
   //Computed Property Names
-  var favoriteNumber = 42;
+//   var favoriteNumber = 42;
   
-  var instructor = {
-    firstName: "Colt"
-  }
+//   var instructor = {
+//     firstName: "Colt"
+//   }
   
-  instructor[favoriteNumber] = "That is my favorite!"
+//   instructor[favoriteNumber] = "That is my favorite!"
+ /* Write an ES2015 Version */
+let favoriteNumber = 42;
+const instructor = {
+    firstName: 'Colt',
+    [favoriteNumber]: 'That is my favorite!'
+}
+
   //Object Methods
   var instructor = {
       firstName: "Colt",
@@ -25,7 +35,17 @@ function createInstructor(firstName, lastName){
         return this.firstName + " says bye!";
       }
     }
-  
+   /* Write an ES2015 Version */
+const instructor = {
+    firstName: 'Colt',
+    sayHi(){
+        return 'Hi!'
+    },
+    sayBye(){
+        return `${this.firstName} says bye!`
+    }
+}
+
   //   createAnimal function
   // Write a function which generates an animal object. The function should accepts 3 arguments:
   
@@ -41,3 +61,12 @@ function createInstructor(firstName, lastName){
   const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
   // {species: "sheep", bleet: ƒ}
   s.bleet() //"BAAAAaaaa"
+   /* Write an ES2015 Version */
+   function createAnimal(species, verb, noise){
+    return {
+        species,
+        [verb]: function(){
+            return noise
+        }
+    }
+}
